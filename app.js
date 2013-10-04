e886895af2b4abafc0c24ef9d0d1b3c8cea1ce27
David Fekke <david@fekke.com>
2013-10-04 05:42:00 -0400

    New favicon

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 
 // development only
 if ('development' == app.get('env')) {
