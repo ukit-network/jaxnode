40ae01cf044869547779cbe3271fa676a0cf9da5
David Fekke <david@fekke.com>
2015-12-11 17:05:06 -0500

    Adding fakes and basic structure with DI

--- services/jaxnode-service.js
+++ services/jaxnode-service.js
@@ -1,3 +1,21 @@
 "use strict";
 
 // TODO: modurualize the data to make the testing faster
+var Service = function Service(meetupdata, twitterdata) {
+	this.Meetup = meetupdata;
+	this.Twitter = twitterdata;
+}
+
+Service.prototype.getNextMeetup = function getNextMeetup() {
+	
+};
+
+Service.prototype.getTweets = function getTweets() {
+	
+};
+
+function create(meetupdata, twitterdata) {
+	return new Service(meetupdata, twitterdata); 
+}
+
+module.exports = create;
