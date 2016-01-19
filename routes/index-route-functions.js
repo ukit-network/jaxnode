33036ca945af224028b4df9ac75ed041a56a7c5e
davidfekke <david@fekke.com>
2016-01-18 22:55:12 -0500

    Adding new fakes to test errors

--- routes/index-route-functions.js
+++ routes/index-route-functions.js
@@ -7,11 +7,25 @@ exports.index = function index(req, res) {
     req.service.getNextMeetup(function callback(err, results) {
         if (err) {
             console.log('problem with request: ' + err);
+            res.status(500).render('error', {
+                message: 'No app exists',
+                error: {
+                    status: '500',
+                    stack: 'App Error'
+                }
+            });
         } else {
             var meetingArray = results;
             req.service.getTweets(function cb(err2, tweetResults) {
                 if (err2) {
                     console.log('problem with request: ' + err);
+                    res.status(500).render('error', {
+                        message: 'No tweets',
+                        error: {
+                            status: '500',
+                            stack: 'App Twitter Error'
+                        }
+                    });
                 } else {
                     var displayMeetup = Object.keys(meetingArray).length !== 0;
                     var displayTweets = tweetResults.tweets.length !== 0;
@@ -37,6 +51,13 @@ exports.code = function code(req, res) {
     req.getCode(function callback(err, results) {
         if (err) {
             console.log('problem with request: ' + err);
+            res.status(500).render('error', {
+                message: 'problem with request',
+                error: {
+                    status: '500',
+                    stack: 'problem with request'
+                }
+            });
         } else {
             res.render('code', { title: 'Jax Node GitHub code', repos: results.repos });
         }
@@ -47,6 +68,7 @@ exports.api = function api(req, res) {
     req.service.getNextMeetup(function callback(err, results) {
         if (err) {
             console.log('problem with request: ' + err);
+            res.status(500).send({ meeting: 'Error occured' });
         } else {
             res.send({ meeting: results });
         }
---
+++ tests/testerrorroutes.js
@@ -0,0 +1,81 @@
+'use strict';
+var request = require('supertest');
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('../routes/index');
+var routesForApps = require('../routes/appsroutes');
+
+var twitterdata = require('../fakes/twittererrorfake.js');
+var meetupdata = require('../fakes/meetuperrorfake.js');
+var githubData = require('../fakes/githuberrorfake.js');
+var servicefactory = require('../services/jaxnode-service.js');
+var service = servicefactory(meetupdata, twitterdata);
+var path = require('path');
+
+var app = express();
+var hbs = require('express-hbs');
+var hbsHelpers = require('../services/hbsHelpers.js');
+hbsHelpers(hbs);
+
+app.set('port', process.env.PORT || 3000);
+app.engine('hbs', hbs.express4());
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../views'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var exposeService = function (req, resp, next) {
+    req.service = service;
+    req.getCode = githubData;
+    next();
+};
+
+app.use('/', exposeService, routes);
+app.use('/apps', routesForApps);
+
+describe('Routes', function () {
+    describe('GET Index', function () {
+        it('responds to /', function testHomepage(done) {
+            request(app)
+                .get('/')
+                //.expect('Content-Type', /text\/html/)
+                .expect(500, done);
+        });
+    });
+
+    describe('GET Code', function () {
+        it('responds to /Code', function testCode(done) {
+            this.timeout(10000);
+            request(app)
+                .get('/Code')
+                .expect('Content-Type', /text\/html/)
+                .expect(500, done);
+        });
+    });
+
+    describe('GET Api', function () {
+        this.timeout(10000);
+        it('responds to /api', function testApi(done) {
+            request(app)
+                .get('/api')
+                //.expect('Content-Type', /application\/json/)
+                .expect(500, done);
+        });
+    });
+
+    describe('GET Apps', function () {
+        it('responds to /Apps', function testApps(done) {
+            request(app)
+                .get('/apps')
+                .expect('Content-Type', /text\/html/)
+                .expect(200, done);
+        });
+    });
+
+});
